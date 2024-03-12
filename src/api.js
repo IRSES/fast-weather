a912801a30d376db86eee68087f09b088719dfbb
IRSES <barmen3456besdna@gmail.com>
2024-03-12 20:47:01 +0200

    [AS-1] v1.00

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5451625d21msh6c43d9d47fc70e5p129864jsn901da3d9d48e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"; // Current weather API
+export const WEATHER_API_KEY = "8ec55e2b1932012f971a6116276557e9"; 
